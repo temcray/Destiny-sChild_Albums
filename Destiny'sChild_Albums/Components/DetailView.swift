4645d602c7e5f7bd7345be11debf87abbf69bce9
Tatiana Mccray <ejamison06@gmail.com>
2026-02-20 14:01:04 -0600

    hhgg

---
+++ Destiny'sChild_Albums/Components/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  Destiny'sChild_Albums
+//
+//  Created by Tatiana6mo on 2/20/26.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView()
+}
---
+++ Destiny'sChild_Albums/Model/Album.swift
@@ -0,0 +1,18 @@
+//
+//  Album.swift
+//  Destiny'sChild_Albums
+//
+//  Created by Tatiana6mo on 2/20/26.
+//
+
+import SwiftUI
+
+struct Album: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Album()
+}
