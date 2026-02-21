155564629478a905c524ae65dbf10878d4046aa6
Tatiana Mccray <ejamison06@gmail.com>
2026-02-20 18:30:32 -0600

    assingment 4

---
+++ Destiny'sChild_Albums/Components/EditView.swift
@@ -0,0 +1,18 @@
+//
+//  EditView.swift
+//  Destiny'sChild_Albums
+//
+//  Created by Tatiana6mo on 2/20/26.
+//
+
+import SwiftUI
+
+struct EditView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditView()
+}
